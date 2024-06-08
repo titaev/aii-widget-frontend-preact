d40c6a99236b25b495753f3e148be7e839e9faac
Alexandr Kasatov <slabass@mail.ru>
2024-06-08 12:55:50 +0600

    answer

--- src/components/layouts/MiniPageLayout.tsx
+++ src/components/layouts/MiniPageLayout.tsx
@@ -3,15 +3,15 @@ import {ModalWrapper} from "@src/components/sections/ModalWrapper";
 import {MINI_PAGE_CLASS, MINI_PAGE_WRAPPER_CLASS, WIDGET_CONTAINER_CLASS} from "@src/classNames";
 import {WidgetTitle} from "@src/components/WidgetTitle";
 import {AiForm} from "@src/components/sections/AiForm";
+import {Answer} from "@src/components/sections/Answer";
 
 export const MiniPageLayout = () => {
     return <div className={`${WIDGET_CONTAINER_CLASS} ${MINI_PAGE_CLASS}`}>
         <WidgetTitle/>
         <div className={MINI_PAGE_WRAPPER_CLASS}>
             <AiForm/>
-            <h3>answer</h3>
+            <Answer/>
         </div>
-
         {!!$modalContent.value && <ModalWrapper/>}
     </div>
 }
