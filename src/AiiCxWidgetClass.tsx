e795dab98702d2d8ad38d42d3a77ac048c43a3a7
Alexandr Kasatov <slabass@mail.ru>
2024-12-13 12:10:11 +0600

    prevent memory leaks if widget mounts and destroys many times

--- src/AiiCxWidgetClass.tsx
+++ src/AiiCxWidgetClass.tsx
@@ -1,16 +1,41 @@
 import { render } from 'preact';
 import { App } from '@src/App';
+import { WidgetAppWrapper } from '@src/WidgetAppWrapper';
+import { signal } from '@preact/signals';
 
 export class AiiCxWidget {
   rootNode: Element | null = null;
+  isMounted = false;
+  isFirstMount = true;
+  mountSignal = signal(true);
 
   mount({ node, id, isPreviewMode }: { node: Element; id: string; isPreviewMode?: boolean }) {
-    this.rootNode = node;
-    node.attachShadow({ mode: 'open' });
-    render(<App widgetId={id} isPreviewMode={!!isPreviewMode} />, node.shadowRoot);
+    if (this.isMounted) {
+      return;
+    }
+
+    if (this.isFirstMount) {
+      this.isFirstMount = false;
+      const rootNode = document.createElement('div');
+      rootNode.attachShadow({ mode: 'open' });
+      node.appendChild(rootNode);
+      this.rootNode = rootNode;
+      this.isMounted = true;
+      render(
+        <WidgetAppWrapper mountSignal={this.mountSignal}>
+          <App widgetId={id} isPreviewMode={!!isPreviewMode} />
+        </WidgetAppWrapper>,
+        this.rootNode.shadowRoot,
+      );
+    } else {
+      this.mountSignal.value = true;
+    }
   }
 
   destroy() {
-    console.log('destroy');
+    //TODO убрать теги style
+    //TODO закрыть сокет соединение
+    this.isMounted = false;
+    this.mountSignal.value = false;
   }
 }
